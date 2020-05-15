9cf5209af5691481a327d70747ca255d4f6229cc
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-05-15 07:36:15 +0430

    remove tab added

--- LastRequestsGUI.java
+++ LastRequestsGUI.java
@@ -15,14 +15,12 @@ import java.awt.event.KeyEvent;
  * 
  * 
  * @author Mohammad Mahdi Malmasi
- * @version 0.1.0
+ * @version 0.1.1
  */
-public class LastRequestsGUI 
+public class LastRequestsGUI extends JPanel
 {
             /*  Fields  */
 
-    // the main panel of the last requests part
-    private JPanel lastRequestsMainPanel;
 
     // Insomnia text label
     private JLabel insomniaLabel;
@@ -30,14 +28,23 @@ public class LastRequestsGUI
     // requests groups tabbed pane
     private JTabbedPane requestGroups;
 
-    // add tab panel
-    private JPanel addTabPanel;
+    // groups name combo box
+    private JComboBox<String> groupsComboBox;
+
+    // add group tab panel
+    private JPanel addGroupTabPanel;
 
     // get new tab name Text field
     private JFormattedTextField getNewTabNameField;
 
-    // add new tab button
-    private JButton addButton;
+    // add new group button
+    private JButton addGroupButton;
+
+    // remove group tab panel
+    private JPanel removeGroupTabPanel;
+
+    // remove group button
+    private JButton removeGroupButton;
 
     // last requests panel
     private JPanel lastRequestsPanel;
@@ -65,14 +72,14 @@ public class LastRequestsGUI
      */
     public LastRequestsGUI(Color bgColor)
     {
+        // set the super class
+        super();
+        this.setLayout(new BorderLayout()); // set layout manager
+        
+
+
         // set background color
         backgroundColor = bgColor;
-       
-
-
-        // set the main panel
-        lastRequestsMainPanel = new JPanel(); // create new panel
-        lastRequestsMainPanel.setLayout(new BorderLayout()); // set layout manager
 
 
 
@@ -84,13 +91,15 @@ public class LastRequestsGUI
         insomniaLabel.setForeground(Color.WHITE); // set text color
         insomniaLabel.setBackground(new Color(102, 96, 178)); // set back ground color
         insomniaLabel.setOpaque(true); // apply color changes
-        lastRequestsMainPanel.add(insomniaLabel, BorderLayout.NORTH); // add to the main panel
+        this.add(insomniaLabel, BorderLayout.NORTH); // add to the main panel
         
         
         
         // set the '+' tab
-        addTabPanel = new JPanel(); // create new panel
-        addTabPanel.setLayout(new FlowLayout()); // set the layout manager
+        addGroupTabPanel = new JPanel(); // create new panel
+        addGroupTabPanel.setLayout(new FlowLayout()); // set the layout manager
+        addGroupTabPanel.setBackground(backgroundColor); // set background color
+        addGroupTabPanel.setOpaque(true); // apply color changes
         
 
 
@@ -99,15 +108,37 @@ public class LastRequestsGUI
         getNewTabNameField.setForeground(new Color(71, 71, 69)); // set foreground color
         getNewTabNameField.setPreferredSize(new Dimension(190, 33)); // set size
         getNewTabNameField.setFont(insomniaLabel.getFont().deriveFont(15.0f)); // set text size
-        addTabPanel.add(getNewTabNameField); // add text field
+        addGroupTabPanel.add(getNewTabNameField); // add text field
 
 
 
         // set add button
-        addButton = new JButton(" Add "); // creat new button
-        addButton.setPreferredSize(new Dimension(70, 35)); // set size
-        addTabPanel.add(addButton); // add button
+        addGroupButton = new JButton(" Add "); // creat new button
+        addGroupButton.setPreferredSize(new Dimension(70, 35)); // set size
+        addGroupTabPanel.add(addGroupButton); // add button
 
+
+
+        // set the '-' tab
+        removeGroupTabPanel = new JPanel(); // create new panel
+        removeGroupTabPanel.setLayout(new FlowLayout()); // set layout manager
+        removeGroupTabPanel.setBackground(backgroundColor); // set background color
+        removeGroupTabPanel.setOpaque(true); // apply color changes
+
+
+
+        // set groups combo box
+        groupsComboBox = new JComboBox<>(); // create new combo box
+        groupsComboBox.setPreferredSize(new Dimension(193, 33)); // set size
+        groupsComboBox.setFont(groupsComboBox.getFont().deriveFont(15.0f)); // set text size
+        removeGroupTabPanel.add(groupsComboBox); // add groups combo box
+
+
+
+        // set remove button
+        removeGroupButton = new JButton(" Remove "); // creat new button
+        removeGroupButton.setPreferredSize(new Dimension(85, 35)); // set size
+        removeGroupTabPanel.add(removeGroupButton); // add button
 
         
         // set last requests panel
@@ -123,7 +154,8 @@ public class LastRequestsGUI
         requestGroups.setBackground(backgroundColor); // set color
         requestGroups.setOpaque(true); // apply color changes
         requestGroups.addTab("last requests", lastRequestsPanel); 
-        requestGroups.addTab(" + ", addTabPanel); 
-        lastRequestsMainPanel.add(requestGroups, BorderLayout.CENTER); // add to the main panel
+        requestGroups.addTab(" + ", addGroupTabPanel); 
+        requestGroups.addTab(" - ", removeGroupTabPanel);
+        this.add(requestGroups, BorderLayout.CENTER); // add to the main panel
     }
 }
