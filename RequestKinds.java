1bc552c7fa62991a2098bedd5cfa0a8c8e4d72ea
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-05-22 15:30:19 +0430

    HTTP requset kinds enum added

---
+++ RequestKinds.java
@@ -0,0 +1,18 @@
+
+
+
+/** 
+ * This enum reprsent HTTP requset kinds
+ * 
+ * @author Mohammad Mahdi Malmasi
+ * @version 0.1.0
+ */
+public enum RequestKinds 
+{
+    // kinds
+    GET,
+    POST,
+    PUT,
+    PATCH,
+    DELET;
+}
