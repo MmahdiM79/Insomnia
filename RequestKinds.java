85229548c88ccfb60b9a36dfc91b4bf219bbe1cc
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-05-22 15:56:26 +0430

    getKind method added

--- RequestKinds.java
+++ RequestKinds.java
@@ -5,7 +5,7 @@
  * This enum reprsent HTTP requset kinds
  * 
  * @author Mohammad Mahdi Malmasi
- * @version 0.1.0
+ * @version 0.1.1
  */
 public enum RequestKinds 
 {
@@ -15,4 +15,42 @@ public enum RequestKinds
     PUT,
     PATCH,
     DELET;
+
+
+
+            /*  Methods  */
+
+    /**
+     * This class return the kind of the given {@code String}
+     * 
+     * 
+     * @param kind : name of the kind
+     * @return {@code null} if the given {@code String} dosen't mean any kind
+     */
+    public static RequestKinds getKind(String kind)
+    {
+        kind = kind.replaceAll(" ", "").toUpperCase();
+
+
+        switch (kind)
+        {
+            case "GET":
+                return GET;
+
+            case "POST":
+                return POST;
+
+            case "PUT":
+                return PUT;
+
+            case "PATCH":
+                return PATCH;
+
+            case "DELET":
+                return DELET;
+
+            default:
+                return null;
+        }
+    }
 }
