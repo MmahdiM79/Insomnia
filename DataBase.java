fa5f05ea24c07ad3ba3dc9d406c0a1d949139d84
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-05-25 15:12:58 +0430

    data base class added.

---
+++ DataBase.java
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+/**
+ * This class handle storing requests
+ * 
+ * 
+ * @author Mohammad Mahdi Malmasi
+ * @version 0.0.0
+ */
+public final class DataBase 
+{
+            /*  Fields  */
+
+    // main folder of store age
+    private static final String MAIN_FOLDER = "./DataBase/";
+
+    // save the request that dosen't belong to any group
+    private static final String LAST_REQUESTS_FOLDER = MAIN_FOLDER + "lastRequestsFolder/";
+
+    // default name for files
+    private static final String DEFAULT_FILE_NAME = "outputـ";
+
+    // files format
+    private static final String FORMAT = ".rqstdata";
+
+
+
+
+
+
+
+
+}
