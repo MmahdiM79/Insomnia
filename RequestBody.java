716c0da21a53d1808902461e8c34ca2b8b948a19
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-06-02 14:34:13 +0430

    error method updated

--- RequestBody.java
+++ RequestBody.java
@@ -66,7 +66,7 @@ public abstract class RequestBody
      */
     protected void error()
     {
-        System.out.println(" can not make body or write on connectoin output stream - ( check Jurl --help )");
+        System.out.println(" An error occurs while trying to set requset body - ( check Jurl --help )");
         System.exit(0);
     }
 }
