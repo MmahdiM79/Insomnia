cdc4d9cd38abc0cdf01ca9b633b4f7d91c9caf08
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-06-02 09:41:02 +0430

    error method updated

--- RequestBody.java
+++ RequestBody.java
@@ -58,12 +58,13 @@ public abstract class RequestBody
 
 
 
-
-    // this method close the app 
-    // use this method on errors
+    /**
+     * this method close the app 
+     * use this method on errors
+     */
     protected void error()
     {
-        System.out.println(" your given datas are invalid - ( check Jurl --help )");
+        System.out.println(" can't write on connectoin output stream - ( check Jurl --help )");
         System.exit(0);
     }
 }
