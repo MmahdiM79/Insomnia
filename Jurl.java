8add0181c784d266237878b0c7a731f4163990b7
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-06-10 20:05:57 +0430

    help and remove optino methods added and some minnor changes

--- Jurl.java
+++ Jurl.java
@@ -13,7 +13,7 @@ import java.net.http.*;
  * 
  * 
  * @author Mohammad Mahdi Malmasi
- * @version 0.1.4
+ * @version 0.1.7
  */
 public class Jurl 
 {
@@ -24,7 +24,6 @@ public class Jurl
 
     // app difined args
     private static String[]  options =  {"-m", "--method",   // set kind of the request 
-                                         "-b", "--body",     // set kind of the request body
                                          "-h", "--headers",  // set headers of the request
                                          "-d", "--data",     // set datas for message body
                                          "-o", "--output",   // save response body to given file
@@ -32,7 +31,8 @@ public class Jurl
                                          "-q", "--query",    // add a query to request
                                          "-u", "--upload",   // send a file as request body
                                          "-l", "--list",     // show the list of the request groups
-                                         "-r", "--remove",   // remove a request or group
+                                         "rm", "--remove",   // remove a request or group
+                                         "so", "--save-over",// override saving
                                                "--send",     // send again the given requests
                                                "--help",     // open help text
                                          "-f",               // follow redirect
@@ -41,8 +41,6 @@ public class Jurl
                                          "--GUI--"           // set the outputs to GUI
                                         }; 
 
-    // kinds of the request body
-    private static String[] bodyKinds = {"form-data", "json", "binary-file"};
 
     //show outputs on terminal or not
     private static boolean terminal = true;
@@ -52,6 +50,9 @@ public class Jurl
 
     // address of file to upload
     private static String uploadFilePath = null;
+
+    // check that user do anything or not
+    private static boolean doSomeThing = false;
 
 
 
@@ -81,16 +82,59 @@ public class Jurl
 
     public static void main(String[] inputs) throws IOException
     {
-        //String[] inputss = {"yahoo.com", "-i", "-o"};
+        //String[] inputss = {"google.com", "-i", "-s"};
+
         getArgs(inputs);
-       
+        if (args.size() == 0)
+        {
+            System.out.println("Jurl:   no option given");
+            System.out.print("Jurl:   try 'java Jurl --help' for more information\n");
+            return;
+        }
+
+
         if (args.contains("--GUI--"))
             terminal = false;
+        else    
+        {
+            System.out.println("\033[H\033[2J");
+            System.out.flush();
+        }
 
         init();
 
+
+
+
+        if (args.contains("rm") || args.contains("--remove"))   
+        {
+            doSomeThing = true;
+            removeOption();
+        }
+
         
+        if (args.contains("--help"))
+        {
+            showHelp();
+            return;
+        }
+
+        if (args.contains("-l") || args.contains("--list"))
+        {
+            Out.list(DataBase.getRequests());
+            return;
+        }
+
+    
+        if (isArgDefined(args.get(0)) && !doSomeThing)
+            Out.printErrors("urlFirst");
+
+        else if (isArgDefined(args.get(0)) && doSomeThing)
+            return;
+
         url = args.get(0);
+
+
         checkInputsAndSetFields();
 
 
@@ -112,7 +156,7 @@ public class Jurl
         
 
         if (args.contains("-s") || args.contains("--save"))
-            request.saveToFile();
+            saveOption();
 
 
         request.run();
@@ -179,32 +223,8 @@ public class Jurl
 
         bodyDatas = checkArg("-d", "--data", true);
 
-
-
-        requestBodyKind = checkArg("-b", "--body", true);
-        if (requestBodyKind != null)
-        {
-            boolean isGivenKindValid = false;
-                for (String kind : bodyKinds)
-                    if (requestBodyKind.equals(kind))
-                        isGivenKindValid = true;
-    
-
-                if (!isGivenKindValid)
-                    Out.printErrors("invalidBodyKind");
-        }
-
-        if (requestBodyKind == null && bodyDatas != null)
-            Out.printErrors("noBodyKind");
-
-        if (requestBodyKind != null && bodyDatas == null)
-            Out.printErrors("noData");
-
-        
-        
+ 
         query = checkArg("-q", "--query", true);
-
-        checkArg("-r", "--remove", false);
 
 
         if (args.contains("-o") || args.contains("--output"))
@@ -226,6 +246,50 @@ public class Jurl
             if (!checkOutputFileName.isDirectory() && checkOutputFileName.getParent() != null)
                 Out.printErrors("outputNotExist");
         }
+    }
+
+
+    // this method handel the remove option
+    private static void removeOption()
+    {
+        String selectedArg = args.contains("rm") ? "rm" : "--remove";
+
+
+        ArrayList<String> rmEntries = new ArrayList<>();
+        for (int i = args.indexOf(selectedArg)+1; i < args.size(); i++)
+        {
+            if (!isArgDefined(args.get(i)))
+            {
+                if (args.get(i).length() > 2)
+                    Out.printErrors("badEntry", selectedArg);
+
+
+                int removeCode;
+                try { removeCode = Integer.parseInt(args.get(i)); }
+                catch (NumberFormatException e) { Out.printErrors("badEntry", selectedArg); }
+
+                rmEntries.add(args.get(i));
+            }
+
+            else if (rmEntries.size() == 0)
+                Out.printErrors("noEntery", selectedArg);
+        }
+
+
+        for (String code : rmEntries)
+        {
+            if (code.length() == 2)
+            {
+                try{ DataBase.removeRequest(code); }
+                catch (IndexOutOfBoundsException e) { Out.printErrors("badEntery", selectedArg); }
+            }
+            else
+            {
+                try{ DataBase.removeGroup(code); }
+                catch (IndexOutOfBoundsException e) { Out.printErrors("badEntery", selectedArg); }
+            }
+        }
+                
     }
 
     
@@ -271,7 +335,7 @@ public class Jurl
 
 
     // this method check that the users used a option true or not and return the arg entery
-    private static String checkArg(String argShort, String argLong, Boolean setEntery)
+    private static String checkArg(String argShort, String argLong, boolean setEntery)
     {
         if (args.contains(argShort) || args.contains(argLong))
         {
@@ -304,9 +368,89 @@ public class Jurl
 
 
     // this method end app if an option given to times
-    public static void checkToTime(String s1, String s2)
+    private static void checkToTime(String s1, String s2)
     {
         if (args.contains(s1) && args.contains(s2))
             Out.printErrors("twotime", s1 + " and " + s2);
+    }
+
+
+    private static void showHelp()
+    {
+        System.out.println(
+        "\n \t\t\t\t ------------------------------------------------- \n\n" + 
+        "Jurl is a tool to sent http request over terminal.\n" + 
+        "this tool support GET, POST, PUT and DELETE methods for requests.\n\n" +
+        "contact us:  https://github.com/MmahdiM79 \n\n" +
+
+        "java Jurl < url - if you want send new request > < options >\n\n" +
+
+        "optins:\n\n" + 
+
+        "\t -m, --method    <method to set>\n\n" + 
+        "\t\t use this option to set your request method.\n" +
+        "\t\t if you don't use this option, request method will set as GET.\n" + 
+        "\t\t supported methods: GET, POST, PUT, DELETE.\n\n\n" +
+
+        "\t -q, --query    \"key=value&key1=value1& ...\" \n\n" +
+        "\t\t use this option to set a query to your request. \n" +
+        "\t\t some example for bad entry:  \"=value\", \"=\" \n\n\n" +
+
+        "\t -h, --headers    \"key:value,key1:value1, ...\" \n\n" +
+        "\t\t use this option to set the headers of the new request.\n" + 
+        "\t\t your given key or value can't be empty.\n" + 
+        "\t\t some examples for bad entry:  \":value\", \"key:\", \":\" \n\n\n" +
+
+        "\t -i \n" +
+        "\t\t use this option if you want to see the response details.\n\n\n" +
+
+        "\t -f \n" +
+        "\t\t if you use this option, Jurl will follow redirects. \n\n\n" +
+
+        "\t -o, --output    <file neme, file absolute path, none>\n\n" +
+        "\t\t use this method to save response body in a file.\n" +
+        "\t\t outputs will save in outputs folder in app folder.\n\n\n" +
+
+        "\t -s, --save    <group name, none> <request name, none> \n\n" + 
+        "\t\t use this option to save your request details. \n" + 
+        "\t\t if you don't give the group name, your request will save in last requests foder. \n" +
+        "\t\t if you don't give the request name, your request will save with a default name. \n\n\n" +
+
+        "\t so, --save-over \n" +
+        "\t\t if you use this file option, your request will save with given name and group, \n" +
+        "\t\t even a request with given name and group is available. \n" +
+        "\t\t this option should use with -s or --save. \n\n\n" +
+
+        "\t -d, --data    \"key=value&key1=value1& ...\" \n\n" +
+        "\t\t use this method to give your datas for request body. \n" +
+        "\t\t your given datas with this option, will send as form-data. \n" + 
+        "\t\t with this option, your body request kind will set as Form Data. \n" +
+        "\t\t if you want send a binary file with your request in Form Data kind,  \n" +
+        "\t\t add '(FILE)' at the end of your key and set your file path as value of this key \n" +
+        "\t\t some example for bad entry: \"key=\", \"=value\", \"=\", \"key\" \n\n\n" +
+
+        "\t -j, --json    {key:value,key1:value1, ...} \n\n" + 
+        "\t\t use this option to send a JSON with your request. \n" +
+        "\t\t if you use this option your body kind will set as JSON. \n\n\n" +
+
+        "\t -u, --upload    <file absolute path> \n\n" +
+        "\t\t use this option to send a file with your request. \n" +
+        "\t\t if you use this option your request body kind, will set as binary file. \n\n\n" + 
+
+        "\t -l, --list \n" +
+        "\t\t this metod will show you a list of saved requests. \n\n\n" +
+
+        "\t rm, --remove    <group number><request number, none> \n\n" +
+        "\t\t use this option to remove a saved request. \n" +
+        "\t\t 'java Jurl rm 0' means that remove group number 0. \n" +
+        "\t\t 'java Jurl rm 01' means that remove request number 1 from group number 0. \n" + 
+        "\t\t this option only gives one code (remove a group or remove a request) at the same time. \n\n\n" +
+
+        "\t --send    <request code> \n\n" +
+        "\t\t use this request to send a saved request. \n" +
+        "\t\t 'java Jurl --send 00' means that re-send request number 0 form group number 0" + 
+
+        "\n\n \t\t\t\t ------------------------------------------------- \n\n"
+        );
     }
 }
